2eebff6e8561c11c5a531f3e10bb0bc34e170a9f
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-19 00:55:41 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,6 +13,12 @@ class MP3Importer
   end 
   
   def files 
-end 
+    Dir.entries(@path).select {|file| file.include?(".mp3")}
+  end 
+  
+  def import 
+    files.each do |file|
+      Song.new_by_filename(file)
+  end
   
 end 
