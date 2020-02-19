458e45977f9178da3f41e70864acf90678fc2839
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-19 00:44:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,14 +26,21 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    
+    self.find_by_name(name) || self.create_by_name(name)
   end 
   
   def self.find_by_name(name)
-    
+    @@all.find {|artist| artist.name == name}
   end
   
   def self.create_by_name(name)
+    new_artist = Artist.new(name)
+    new_artist
+  end
+  
+  def print_songs 
+    songs.each {|song| puts song.name}
+    end
   end
   
 end
